3836f9854811e86f8e5d6be59ac8f6f86a200619
Chukwuebuka Cliff <klifiam@gmail.com>
2021-07-02 10:38:16 +0100

    Added Product Data

---
+++ src/components/Products/data.js
@@ -0,0 +1,27 @@
+import product1 from '../../images/product-1.jpg'
+import product2 from '../../images/product-2.jpg'
+import product3 from '../../images/product-3.jpg'
+
+export const productData = [
+  {
+    img: product1,
+    alt: 'Faves',
+    name: 'Lunch Pack',
+    desc: 'Rice, beans & atadindin x turkey',
+    button: '₦2,500',
+  },
+  {
+    img: product2,
+    alt: 'Faves',
+    name: 'Yam Pottage',
+    desc: 'Asaro x Fish x Ponmo',
+    button: '₦1,800',
+  },
+  {
+    img: product3,
+    alt: 'Faves',
+    name: 'Lunch Pack',
+    desc: 'Fried rice x turkey',
+    button: '₦3,000',
+  },
+]
